2de126b293a3756465787cd3aa02c22c4ed8712a
WelbyC <welby.c@yahoo.com>
2018-11-30 13:35:25 -0800

    Update and rename Aux.js to ReactAux.js

--- src/hoc/Aux/ReactAux.js
+++ src/hoc/Aux/ReactAux.js
@@ -1,5 +1,5 @@
 // we just want to wrap something and output it to fulfill requirment of having wrapped content
 // No jsx, so we don't have to import react from React
-const aux = (props) => props.children;
+const ReactAux = (props) => props.children;
 
-export default aux;
+export default ReactAux;
