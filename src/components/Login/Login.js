8c0aab206abaac11741de2f7b5dcfdae5a6deaab
Ahmed Darkazanli <adarkazanli2@gmail.com>
2018-12-02 23:37:32 -0800

    added content to the login modal

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,7 +5,25 @@ import styles from './Login.module.css'
 const login = (props) => {
     return (
         <Aux>
-            login content
+
+            <h3 className={styles['login-text']}>Login</h3>
+            <div className={styles['username-text']}>Username</div>
+            <input type="text" placeholder="Enter Username" className={styles['username-input']} />
+            <div className={styles['password-text']}>Password</div>
+            <input type="password" placeholder="Enter Password" className={styles['password-input']} />
+            <button type="submit" className={styles['login-button']}>Login</button>
+            <label>
+                <input type="checkbox" checked="checked" name="remember" className={styles['checkbox']} /> Remember me
+            </label>
+
+
+            <div>
+                <button type="button" className={styles["cancelbtn"]}>Cancel</button>
+                <span className={styles['psw']}>Forgot&nbsp;
+                    <a href="#">password?</a>
+                </span>
+            </div>
+
         </Aux>
     );
 }
